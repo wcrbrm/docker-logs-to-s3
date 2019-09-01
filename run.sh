a71eb97bb67a8db54a37ec42dc1a3a7a999df754
wcrbrm <viclopata@gmail.com>
2019-09-01 10:34:43 +0300

    fix input vars

--- run.sh
+++ run.sh
@@ -2,19 +2,17 @@
 set -e
 FN=logs-`date +%Y%m%dT%H%M`
 
-if [[ "$AWS_PROFILE" = "" ]]; then
-    echo "error: please provide AWS_PROFILE variable"
-    exit 1;
+if [[ "$AWS_PROFILE" == "" ]]; then
+    AWS_PROFILE=default
 fi
-if [[ "$S3_PATH" = "" ]]; then
-    echo "error: please provide S3_PATH variable, i.e. s3://example-bucket/igw-logs/"
+if [[ "$S3_LOG_PATH" == "" ]]; then
+    echo "error: please provide S3_LOG_PATH variable, i.e. s3://example-bucket/igw-logs/"
     exit 1;
 fi
 
 mkdir -p out
 [[ -e *.tar.gz ]] && find *.tar.gz -exec rm -rf {} \;
 [[ -e out/*.log ]] && find out/*.log -exec rm -rf {} \; 
-
 
 LIST=$(docker ps --format '{{.ID}};{{.Names}}')
 for L in $LIST; do
@@ -27,8 +25,8 @@ done;
 ls -All out/
 tar zcf $FN.tar.gz out/*.log
 
-aws s3 cp --profile=$AWS_PROFILE $FN.tar.gz $S3_PATH
+aws s3 cp --profile=$AWS_PROFILE $FN.tar.gz $S3_LOG_PATH
 
 rm -f $FN.tar.gz
 rm -rf out
-aws s3 ls --profile=$AWS_PROFILE $S3_PATH
+aws s3 ls --profile=$AWS_PROFILE $S3_LOG_PATH
